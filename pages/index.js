a5441390001aa8114d407b1e48ed552c99f9ee0a
BensonFeng <bensonfeng@outlook.com>
2022-04-24 22:26:36 -0400

    HeroBanner component Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 
+import { Product, FooterBanner, HeroBanner } from "../components";
+
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best selling Products</h2>
-        <p>Speaker of many variations</p>
+        <p>speaker of many variations passages</p>
       </div>
       <div className="products-container">
         {["product1", "product 2"].map((product) => product)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
