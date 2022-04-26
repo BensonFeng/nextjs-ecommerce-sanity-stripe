c1343c873ae5c8ad8fade446446573a06e842f14
BensonFeng <bensonfeng@outlook.com>
2022-04-25 22:14:34 -0400

    useContext inc/dec product

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,11 +8,13 @@ import {
 
 import { client, urlFor } from "../../lib/client";
 import { Product } from "../../components";
+import { useStateContext } from "../../context/StateContext";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-
+  const { decQty, incQty, qty } = useStateContext();
+  console.log({ qty });
   return (
     <div>
       <div className="product-detail-container">
@@ -55,11 +57,11 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num">1</span>
-              <span className="plus">
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
