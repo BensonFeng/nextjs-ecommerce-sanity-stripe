811378f7aaefddd533edf74009cde5c8d90eb98c
BensonFeng <bensonfeng@outlook.com>
2022-04-29 21:58:23 -0400

    Buy Now functionality

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,7 +13,13 @@ import { useStateContext } from "../../context/StateContext";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  };
 
   return (
     <div>
@@ -74,12 +80,13 @@ const ProductDetails = ({ product, products }) => {
             >
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
         </div>
       </div>
+
       <div className="maylike-products-wrapper">
         <h2>You may also like</h2>
         <div className="marquee">
@@ -103,7 +110,6 @@ export const getStaticPaths = async () => {
   `;
 
   const products = await client.fetch(query);
-  console.log({ products });
 
   const paths = products.map((product) => ({
     params: {
